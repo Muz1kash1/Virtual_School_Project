5c81aebb73a2582a6120b8de8d6b9e98a700296a
toppr <Daniil.Shatov@gorparkovki.ru>
2022-09-08 16:28:52 +0300

    feat: комментарии
    Добавлены комментарии к методам объкта Student
    
    #INTERNSHIP-80

--- src/main/java/com/fabit/schoolapplication/domain/student/Student.java
+++ src/main/java/com/fabit/schoolapplication/domain/student/Student.java
@@ -34,22 +34,57 @@ public class Student {
         }
     }
 
+    /**
+     * Создание ученика младше 14 лет.
+     *
+     * @param id               the id
+     * @param name             the name
+     * @param snils            the snils
+     * @param birthCertificate the birth certificate
+     * @param birthday         the birthday
+     * @return the student
+     */
     public static Student create(long id, String name, Snils snils, BirthCertificate birthCertificate, LocalDate birthday) {
         return new Student(id, name, snils, birthCertificate, birthday);
     }
 
+    /**
+     * Создание ученика возрастом минимум 14 лет.
+     *
+     * @param id       the id
+     * @param name     the name
+     * @param snils    the snils
+     * @param passport the passport
+     * @param birthday the birthday
+     * @return the student
+     */
     public static Student create(long id, String name, Snils snils, Passport passport, LocalDate birthday) {
         return new Student(id, name, snils, passport, birthday);
     }
 
+    /**
+     * замена СНИЛСа.
+     *
+     * @param snils the snils
+     */
     public void changeSnils(Snils snils) {
         this.snils = snils;
     }
 
+    /**
+     * Замена свидетельства о рождении.
+     *
+     * @param birthCertificate the birth certificate
+     */
     public void changeBirthCertificate(BirthCertificate birthCertificate) {
         this.birthCertificate = birthCertificate;
     }
 
+    /**
+     * Добавление паспорта.
+     *
+     * @param passport the passport
+     */
     public void addPassport(Passport passport) {
         this.passport = passport;
     }
