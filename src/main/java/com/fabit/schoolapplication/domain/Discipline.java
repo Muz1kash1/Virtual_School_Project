f97fc3dc7ee0b25d6efbf963ffcb9e76dd8f5af7
kalininav <aleksandr.kalinin@gorparkovki.ru>
2022-09-08 12:03:26 +0300

    feat: описаны единые VO
    #INTERNSHIP-85

---
+++ src/main/java/com/fabit/schoolapplication/domain/Discipline.java
@@ -0,0 +1,36 @@
+package com.fabit.schoolapplication.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Discipline {
+
+  RUSSIAN_LANGUAGE("Русский язык"),
+  LITERATURE("Литература"),
+  RUSSIAN_LANGUAGE_AND_LITERATURE("Русский язык и литература"),
+  ENGLISH("Английский язык"),
+  DEUTSCH("Немецкий язык"),
+  FRENCH("Французский язык"),
+  MATHS("Математика"),
+  ALGEBRA("Алгебра"),
+  GEOMETRY("Геометрия"),
+  COMPUTING("Информатика"),
+  PHYSICS("Физика"),
+  CHEMISTRY("Химия"),
+  BIOLOGY("Биология"),
+  HISTORY("История"),
+  SOCIAL_SCIENCE("Обществознание"),
+  GEOGRAPHY("География"),
+  PHYSICAL_EDUCATION("Физическая культура"),
+  MUSIC("Музыка"),
+  DRAWING("Рисование"),
+  TECHNOLOGY("Труд");
+
+  /**
+   * Текстовое представление предмета
+   */
+  private final String textView;
+
+}
