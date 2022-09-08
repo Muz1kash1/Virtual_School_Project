53d5894b42753a33a43651c46063d95199ced0d1
toppr <Daniil.Shatov@gorparkovki.ru>
2022-09-08 17:58:28 +0300

    refactor: добавление стиля кода по google-checks
    #INTERNSHIP-80

--- src/main/java/com/fabit/schoolapplication/domain/student/BirthCertificate.java
+++ src/main/java/com/fabit/schoolapplication/domain/student/BirthCertificate.java
@@ -6,15 +6,15 @@ import lombok.Value;
 @Getter
 @Value
 public class BirthCertificate {
-    String serial;
-    String number;
+  String serial;
+  String number;
 
-    private BirthCertificate(String serial, String number) {
-        this.serial = serial;
-        this.number = number;
-    }
+  private BirthCertificate(String serial, String number) {
+    this.serial = serial;
+    this.number = number;
+  }
 
-    public static BirthCertificate create(String serial, String number) {
-        return new BirthCertificate(serial, number);
-    }
+  public static BirthCertificate create(String serial, String number) {
+    return new BirthCertificate(serial, number);
+  }
 }
--- src/main/java/com/fabit/schoolapplication/domain/student/Student.java
+++ src/main/java/com/fabit/schoolapplication/domain/student/Student.java
@@ -2,90 +2,93 @@ package com.fabit.schoolapplication.domain.student;
 
 import com.fabit.schoolapplication.domain.Passport;
 import com.fabit.schoolapplication.domain.Snils;
-import lombok.Getter;
-
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 
 @Getter
 public class Student {
-    private long id;
-    private String name;
-    private Snils snils;
-    private BirthCertificate birthCertificate;
-    private Passport passport;
-    private LocalDate birthday;
+  private long id;
+  private String name;
+  private Snils snils;
+  private BirthCertificate birthCertificate;
+  private Passport passport;
+  private LocalDate birthday;
 
-    private Student(long id, String name, Snils snils, BirthCertificate birthCertificate, LocalDate birthday) {
-        this.id = id;
-        this.name = name;
-        this.snils = snils;
-        this.birthCertificate = birthCertificate;
-        this.birthday = birthday;
-    }
+  private Student(long id, String name, Snils snils, BirthCertificate birthCertificate,
+                  LocalDate birthday) {
+    this.id = id;
+    this.name = name;
+    this.snils = snils;
+    this.birthCertificate = birthCertificate;
+    this.birthday = birthday;
+  }
 
-    private Student(long id, String name, Snils snils, Passport passport, LocalDate birthday) {
-        this.id = id;
-        this.name = name;
-        this.snils = snils;
-        if (LocalDate.now().getYear() - birthday.getYear() >= 14) {
-            this.passport = passport;
-        } else {
-            throw new IllegalArgumentException();
-        }
+  private Student(long id, String name, Snils snils, Passport passport, LocalDate birthday) {
+    this.id = id;
+    this.name = name;
+    this.snils = snils;
+    if (LocalDate.now().getYear() - birthday.getYear() >= 14) {
+      this.passport = passport;
+    } else {
+      throw new IllegalArgumentException();
     }
+  }
 
-    /**
-     * Создание ученика младше 14 лет.
-     *
-     * @param id               the id
-     * @param name             the name
-     * @param snils            the snils
-     * @param birthCertificate the birth certificate
-     * @param birthday         the birthday
-     * @return the student
-     */
-    public static Student create(long id, String name, Snils snils, BirthCertificate birthCertificate, LocalDate birthday) {
-        return new Student(id, name, snils, birthCertificate, birthday);
-    }
+  /**
+   * Создание ученика младше 14 лет.
+   *
+   * @param id               the id
+   * @param name             the name
+   * @param snils            the snils
+   * @param birthCertificate the birth certificate
+   * @param birthday         the birthday
+   * @return the student
+   */
+  public static Student create(long id, String name, Snils snils, BirthCertificate birthCertificate,
+                               LocalDate birthday) {
+    return new Student(id, name, snils, birthCertificate, birthday);
+  }
 
-    /**
-     * Создание ученика возрастом минимум 14 лет.
-     *
-     * @param id       the id
-     * @param name     the name
-     * @param snils    the snils
-     * @param passport the passport
-     * @param birthday the birthday
-     * @return the student
-     */
-    public static Student create(long id, String name, Snils snils, Passport passport, LocalDate birthday) {
-        return new Student(id, name, snils, passport, birthday);
-    }
+  /**
+   * Создание ученика возрастом минимум 14 лет.
+   *
+   * @param id       the id
+   * @param name     the name
+   * @param snils    the snils
+   * @param passport the passport
+   * @param birthday the birthday
+   * @return the student
+   */
+  public static Student create(long id, String name, Snils snils, Passport passport,
+                               LocalDate birthday) {
+    return new Student(id, name, snils, passport, birthday);
+  }
 
-    /**
-     * замена СНИЛСа.
-     *
-     * @param snils the snils
-     */
-    public void changeSnils(Snils snils) {
-        this.snils = snils;
-    }
+  /**
+   * замена СНИЛСа.
+   *
+   * @param snils the snils
+   */
+  public void changeSnils(Snils snils) {
+    this.snils = snils;
+  }
 
-    /**
-     * Замена свидетельства о рождении.
-     *
-     * @param birthCertificate the birth certificate
-     */
-    public void changeBirthCertificate(BirthCertificate birthCertificate) {
-        this.birthCertificate = birthCertificate;
-    }
+  /**
+   * Замена свидетельства о рождении.
+   *
+   * @param birthCertificate the birth certificate
+   */
+  public void changeBirthCertificate(BirthCertificate birthCertificate) {
+    this.birthCertificate = birthCertificate;
+  }
 
-    /**
-     * Добавление паспорта.
-     *
-     * @param passport the passport
-     */
-    public void addPassport(Passport passport) {
-        this.passport = passport;
-    }
+  /**
+   * Добавление паспорта.
+   *
+   * @param passport the passport
+   */
+  public void addPassport(Passport passport) {
+    this.passport = passport;
+  }
 }
