c4996baaac4dfb6500446925ce2c3f700ec7d736
kalininav <aleksandr.kalinin@gorparkovki.ru>
2022-09-09 14:03:07 +0300

    feat/fix: добавление метод-фабрик для глобальных VO, фикс тестов
    #INTERNSHIP-88

--- src/main/java/com/fabit/schoolapplication/domain/Snils.java
+++ src/main/java/com/fabit/schoolapplication/domain/Snils.java
@@ -5,10 +5,17 @@ import lombok.Getter;
 import lombok.Value;
 
 @Getter
-@AllArgsConstructor
 @Value
 public class Snils {
 
   String numberView;
+
+  private Snils(String numberView) {
+    this.numberView = numberView;
+  }
+
+  public static Snils of(String numberView) {
+    return new Snils(numberView);
+  }
 
 }
--- src/test/java/com/fabit/schoolapplication/domain/student/StudentTest.java
+++ src/test/java/com/fabit/schoolapplication/domain/student/StudentTest.java
@@ -14,7 +14,7 @@ class StudentTest {
   @Test
   @DisplayName("Создается объект ученика младше 14 лет со свидетельством о рождении")
   void createStudent12Years() {
-    Student student = Student.create(1, "Петя Губкин", new Snils("123-343-2231-32"),
+    Student student = Student.create(1, "Петя Губкин", Snils.of("123-343-2231-32"),
         BirthCertificate.create("МК", "55555"), LocalDate.of(2010, 9, 15));
     assertEquals(1l, student.getId());
     assertEquals("Петя Губкин", student.getName());
@@ -25,7 +25,7 @@ class StudentTest {
   @DisplayName("Создается объект ученика старше 15 лет с паспортом")
   void createStudent15Years() {
     Student student =
-        Student.create(1, "Петя Губкин", new Snils("123-343-2231-32"), new Passport("МК", "88888"),
+        Student.create(1, "Петя Губкин", Snils.of("123-343-2231-32"), Passport.of("МК", "88888"),
             LocalDate.of(2007, 9, 15));
     assertEquals(1l, student.getId());
     assertEquals("Петя Губкин", student.getName());
@@ -36,24 +36,24 @@ class StudentTest {
   @DisplayName("Создается объект ученика младше 14 лет c паспортом должен выкинуть ошибку")
   void createStudent12YearsWithPassportShouldThrowIllegalArgumentException() {
     assertThrows(IllegalArgumentException.class,
-        () -> Student.create(1, "Петя Губкин", new Snils("123-343-2231-32"),
-            new Passport("МК", "89292"), LocalDate.of(2010, 9, 15)));
+        () -> Student.create(1, "Петя Губкин", Snils.of("123-343-2231-32"),
+            Passport.of("МК", "89292"), LocalDate.of(2010, 9, 15)));
   }
 
   @Test
   @DisplayName("Изменяем снилс у ученика")
   void changeSnils() {
     Student student =
-        Student.create(1, "Петя Губкин", new Snils("123-343-2231-32"), new Passport("МК", "89292"),
+        Student.create(1, "Петя Губкин", Snils.of("123-343-2231-32"), Passport.of("МК", "89292"),
             LocalDate.of(2007, 9, 15));
-    student.changeSnils(new Snils("777-777-7777-77"));
+    student.changeSnils(Snils.of("777-777-7777-77"));
     assertEquals("777-777-7777-77", student.getSnils().getNumberView());
   }
 
   @Test
   @DisplayName("Изменяем сведетельство о рождении у ученика")
   void changeBirthCertificate() {
-    Student student = Student.create(1, "Петя Губкин", new Snils("123-343-2231-32"),
+    Student student = Student.create(1, "Петя Губкин", Snils.of("123-343-2231-32"),
         BirthCertificate.create("МК", "89292"), LocalDate.of(2007, 9, 15));
     student.changeBirthCertificate(BirthCertificate.create("MK", "99999"));
     assertEquals("99999", student.getBirthCertificate().getNumber());
@@ -64,9 +64,9 @@ class StudentTest {
   @DisplayName("Добавляем пасспорт ученику старе 15 лет")
   void addPassport() {
     Student student =
-        Student.create(1, "Петя Губкин", new Snils("123-343-2231-32"), new Passport("MK", "11111"),
+        Student.create(1, "Петя Губкин", Snils.of("123-343-2231-32"), Passport.of("MK", "11111"),
             LocalDate.of(2007, 9, 15));
-    student.addPassport(new Passport("MK", "99999"));
+    student.addPassport(Passport.of("MK", "99999"));
     assertEquals("99999", student.getPassport().getNumber());
     assertEquals("MK", student.getPassport().getSerial());
   }
