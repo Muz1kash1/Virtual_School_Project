2c4947f0735c45cb66e640e780e5a6e23975401f
SmirnovMA <Mikhail.Smirnov@gorparkovki.ru>
2022-09-09 17:46:07 +0300

    refactor: исправить equalsAndHashCode
    #INTERSHIP-87

--- src/main/java/com/fabit/schoolapplication/domain/educatioprogress/Mark.java
+++ src/main/java/com/fabit/schoolapplication/domain/educatioprogress/Mark.java
@@ -1,5 +1,6 @@
 package com.fabit.schoolapplication.domain.educatioprogress;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 import java.util.regex.Matcher;
@@ -11,6 +12,7 @@ import java.util.regex.Pattern;
  * @author SmirnovMA
  */
 @Getter
+@EqualsAndHashCode
 public final class Mark {
   public static Pattern patternMark =
       java.util.regex.Pattern.compile("^(^[2-5]{1}/[2-5]{1}$|^([2-5]{1})$|^НН/УВ$|^НН$)$");
