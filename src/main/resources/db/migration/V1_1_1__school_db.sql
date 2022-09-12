f763e87f061ed506d30d6fdd11a0042eabcb9fc3
Денис Шевченко <Denis.Shevchenko@gorparkovki.ru>
2022-09-12 16:58:32 +0300

    fix: починены миграции
    #INTERNSHIP-117

--- src/main/resources/db/migration/V1_1_1__school_db.sql
+++ src/main/resources/db/migration/V1_1_1__school_db.sql
@@ -23,34 +23,12 @@ CREATE TABLE school_class
     PRIMARY KEY (id)
 );
 
-CREATE TABLE lesson
-(
-    id BIGSERIAL NOT NULL ,
-    teacher_id BIGSERIAL NOT NULL ,
-    student_id BIGSERIAL NOT NULL ,
-    lesson_id BIGSERIAL NOT NULL ,
-    task_completion_result VARCHAR(255),
-    PRIMARY KEY (id),
-    FOREIGN KEY (teacher_id) REFERENCES teacher(id),
-    FOREIGN KEY (student_id) REFERENCES student(id),
-    FOREIGN KEY (lesson_id) REFERENCES lesson(id)
-);
-
 CREATE TABLE education_progress
 (
     id BIGSERIAL NOT NULL ,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE homework_completion_result
-(
-    id BIGSERIAL NOT NULL ,
-    teacher_id BIGSERIAL NOT NULL,
-    discipline VARCHAR(255),
-    homework_task VARCHAR(255),
-    PRIMARY KEY (id),
-    FOREIGN KEY (teacher_id) references teacher(id)
-);
 
 CREATE TABLE schedule
 (
---
+++ src/main/resources/db/migration/V1_1_4_lesson_db.sql
@@ -0,0 +1,22 @@
+CREATE TABLE lesson
+(
+    id BIGSERIAL NOT NULL ,
+    teacher_id BIGSERIAL NOT NULL ,
+    student_id BIGSERIAL NOT NULL ,
+    lesson_id BIGSERIAL NOT NULL ,
+    task_completion_result VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (teacher_id) REFERENCES teacher(id),
+    FOREIGN KEY (student_id) REFERENCES student(id),
+    FOREIGN KEY (lesson_id) REFERENCES lesson(id)
+);
+
+CREATE TABLE homework_completion_result
+(
+    id BIGSERIAL NOT NULL ,
+    teacher_id BIGSERIAL NOT NULL,
+    discipline VARCHAR(255),
+    homework_task VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (teacher_id) references teacher(id)
+);
