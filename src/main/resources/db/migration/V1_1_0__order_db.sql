890c0fd5128aa8895efac2ffe1720fc092a41975
toppr <Daniil.Shatov@gorparkovki.ru>
2022-09-12 12:31:44 +0300

    feat: миграция бд
    Добавлена миграция на все агрегаты в общем случае.
    
    Изменен файл application.yml
    
    #INTERNSHIP-107

---
+++ src/main/resources/db/migration/V1_1_0__order_db.sql
@@ -0,0 +1,46 @@
+CREATE TABLE student
+(
+    id                BIGSERIAL NOT NULL ,
+    birth_certificate VARCHAR(32),
+    birthday          DATE,
+    name              VARCHAR(255),
+    passport          VARCHAR(32),
+    snils             VARCHAR(32),
+    primary key (id)
+);
+
+CREATE TABLE teacher
+(
+    id BIGSERIAL NOT NULL ,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE school_class
+(
+    id BIGSERIAL NOT NULL ,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE lesson
+(
+    id BIGSERIAL NOT NULL ,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE education_progress
+(
+    id BIGSERIAL NOT NULL ,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE homework_completion_result
+(
+    id BIGSERIAL NOT NULL ,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE schedule
+(
+    id BIGSERIAL NOT NULL ,
+    PRIMARY KEY (id)
+);
