e0e685bae9ff3e91d851eed04a1b68bd127d477d
Калинин Александр <aleksandr.kalinin@gorparkovki.ru>
2022-09-12 09:28:57 +0300

    refactor: рефактор агрегата SchoolClass

--- src/test/java/com/fabit/schoolapplication/domain/schoolclass/SchoolClassNameTest.java
+++ src/test/java/com/fabit/schoolapplication/domain/schoolclass/SchoolClassNameTest.java
@@ -4,27 +4,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ParallelTest {
+public class SchoolClassNameTest {
 
   @Test
   @DisplayName("Создание параллели должно создавать корректную параллель")
   void createParallelTest() {
-    Parallel parallel = Parallel.create(11, "Б");
+    SchoolClassName schoolClassName = SchoolClassName.of(11, "Б");
 
-    Assertions.assertEquals(11, parallel.getGrade());
-    Assertions.assertEquals("Б", parallel.getLitera());
-    Assertions.assertEquals("11Б", parallel.getTextView());
+    Assertions.assertEquals(11, schoolClassName.getParallel());
+    Assertions.assertEquals("Б", schoolClassName.getLitera());
+    Assertions.assertEquals("11Б", schoolClassName.getFullName());
   }
 
   @Test
   @DisplayName("Создание параллели с неверным форматом должно выбрасывать exception")
   void createParallelWithWrongFormatTest() {
     Assertions.assertThrows(IllegalArgumentException.class,
-        () -> Parallel.create(115, "LDAS124"), "Неверный формат параллели");
+        () -> SchoolClassName.of(115, "LDAS124"), "Неверный формат параллели");
     Assertions.assertThrows(IllegalArgumentException.class,
-        () -> Parallel.create(1, "Ъ"), "Неверный формат параллели");
+        () -> SchoolClassName.of(1, "Ъ"), "Неверный формат параллели");
     Assertions.assertThrows(IllegalArgumentException.class,
-        () -> Parallel.create(12, "А"), "Неверный формат параллели");
+        () -> SchoolClassName.of(12, "А"), "Неверный формат параллели");
   }
 
 }
