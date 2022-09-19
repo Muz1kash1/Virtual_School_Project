ab9dbaa7dba1417cfce4849151514d171a9a310d
Шатов Даниил <daniil.shatov@gorparkovki.ru>
2022-09-19 20:24:12 +0300

    feat: Spring Security + тесты

--- src/test/java/com/fabit/schoolapplication/infrastructure/controller/SchoolClassControllerTest.java
+++ src/test/java/com/fabit/schoolapplication/infrastructure/controller/SchoolClassControllerTest.java
@@ -27,12 +27,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 
 @RequiredArgsConstructor
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser
 public class SchoolClassControllerTest {
 
   @Autowired
